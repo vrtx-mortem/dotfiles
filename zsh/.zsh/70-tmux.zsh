5974d3b8670b93802df981aef5cfc8f9b7e007aa
vrtx-mortem <nob_dy@lolwut.me>
2024-11-28 02:10:43 +0100

    (*) tmux sessions made easier

--- zsh/.zsh/70-tmux.zsh
+++ zsh/.zsh/70-tmux.zsh
@@ -1,5 +1,6 @@
 [[ $EUID -eq 0 ]] && return
 [[ ! -z $TMUX ]] && return
+[[ "$TTY" != *"pts"* ]] && return
 
 [[ "$HOSTNAME" == "vortex"         ]] && DEFAULT="work"
 [[ "$HOSTNAME" == "katar"          ]] && DEFAULT="rnd"
@@ -7,12 +8,10 @@
 [[ "$HOSTNAME" == "pentest-monkey" ]] && DEFAULT="work"
 
 
-TMUX_SESSIONS=$(tmux list-session -F '#S' 2>/dev/null)
-[[ $TMUX_SESSIONS = *"work"* ]] || tmux new-session -d -s work -c "${HOME}/projects/current"
-[[ $TMUX_SESSIONS = *"dev"*  ]] || tmux new-session -d -s dev  -c "${HOME}/src"
-[[ $TMUX_SESSIONS = *"rnd"*  ]] || tmux new-session -d -s rnd  -c "${HOME}"
-[[ $TMUX_SESSIONS = *"fun"*  ]] || tmux new-session -d -s fun  -c "${HOME}"
-unset TMUX_SESSIONS
+tmux has-session -t "work" 2>/dev/null || tmux new-session -d -s work -c "${HOME}/projects/current"
+tmux has-session -t "dev" 2>/dev/null  || tmux new-session -d -s dev  -c "${HOME}/src"
+tmux has-session -t "rnd" 2>/dev/null  || tmux new-session -d -s rnd  -c "${HOME}"
+tmux has-session -t "fun" 2>/dev/null  || tmux new-session -d -s fun  -c "${HOME}"
 
 if [[ \
   "$HOSTNAME" == "vortex" || \
